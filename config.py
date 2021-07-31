6dc1ae175d8ef492e2c52fcc17f2479fb97a6a7b
faba9 <87280628+faba9@users.noreply.github.com>
2021-07-31 21:50:37 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,5 @@ DEBUG = True
 # keys of recapcha
 RECAPTCHA_PUBLIC_KEY = '6LenfSwaAAAAALKfxVaaIc7U72SVaNFFQnUJtq5e'
 RECAPTCHA_PRIVATE_KEY = '6LenfSwaAAAAAOUF5UjFzXZBL1o0IMFmU9S2UrsM'
-TESTING = True # to check recapcha automatically
+# to check recapcha automatically
+# TESTING = True
